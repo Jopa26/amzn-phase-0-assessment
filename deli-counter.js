e86dfcd516a30fa7743c4cfa7c4ccdb3d0311623
Luis <jqluis2011@gmail.com>
2022-04-10 15:26:53 -0700

    Completed Assingment

--- deli-counter.js
+++ deli-counter.js
@@ -1,7 +1,42 @@
 // 1. Write your functions here
 
+const katzDeli = ["Ada", "Grace", "Kent"];
 
-// 2. Example Usage
+function line(katzDeli) {
+  const lane = katzDeli.map(function (customer, index) {
+    return `${index + 1}. ${customer}`;
+  });
+  if (lane) {
+    console.log(`The line is currently: ` + deliLine.join(" "));
+  } else {
+    console.log(`The line is currently empty.`);
+  }
+}
+
+function takeANumber(katzDeli, newCustomer) {
+  katzDeli.push(newCustomer);
+  console.log(
+    `Welcome, ${newCustomer}. You are number ${katzDeli.length} in line.`
+  );
+}
+
+function nowServing() {
+  let firstPerson = katzDeli.shift();
+  if (firstPerson) {
+    console.log(`Currently serving ${firstPerson}.`);
+  } else {
+    console.log(`There is nobody waiting to be served!`);
+  }
+}
+
+
+
+
+
+
+
+
+//2. Example Usage
 
 // const katzDeli = []
 
